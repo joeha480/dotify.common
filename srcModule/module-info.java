14bc3e17e7f0cb81b54fe52296305d93b7d69c3e
Joel Håkansson <joel.hakansson@pm.me>
2019-04-01 16:33:08 +0200

    Add Java modules support

---
+++ srcModule/module-info.java
@@ -0,0 +1,11 @@
+module org.daisy.dotify.common {
+	exports org.daisy.dotify.common.braille;
+	exports org.daisy.dotify.common.collection;
+	exports org.daisy.dotify.common.io;
+	exports org.daisy.dotify.common.java;
+	exports org.daisy.dotify.common.splitter;
+	exports org.daisy.dotify.common.text;
+	exports org.daisy.dotify.common.xml;
+	requires java.logging;
+	requires java.xml;
+}
